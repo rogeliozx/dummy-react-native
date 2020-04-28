cd04925479253a8e4ba7e36d39b4d033314c713d
rogelioltz <rogelioltz@gmail.com>
2020-04-28 11:41:09 -0700

    feat: first react native app

---
+++ components/GoalInputs.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Button, Modal } from 'react-native';
+
+const GoalInput = (props) => {
+  const [Goal, setGoal] = useState('');
+  const goalInputHandler = (inputText) => setGoal(inputText);
+  const onAddGoalHandler=()=>{
+    props.onAddGoal(Goal);
+    setGoal('');
+  }
+  return (
+    <Modal visible={props.visible} animationType='slide'>
+      <View style={styles.viwStyle}>
+        <TextInput
+          placeholder='Inserte su nombre'
+          style={styles.inputStyle}
+          onChangeText={goalInputHandler}
+          value={Goal}
+        />
+        <View style={styles.buttonContainer}>
+        <Button style={styles.button} title='Cancel' color='red' onPress={props.onCancel} />
+        <Button style={styles.button} title='ADD' onPress={onAddGoalHandler} />
+        </View>
+      </View>
+    </Modal>
+  );
+};
+const styles = StyleSheet.create({
+  inputStyle: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    width: '80%',
+    marginBottom:10
+  },
+  viwStyle: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer:{
+    flexDirection:'row',
+    justifyContent:'space-around',
+    width:'60%'
+  },
+  button:{
+    width:'40%'
+  }
+});
+
+export default GoalInput;
